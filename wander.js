f2793877a77364e39d9503abb95ae0ef1887e79f
river <rrivverr@protonmail.com>
2022-06-05 02:02:42 -0400

    Fix formatting for phone number entry

--- wander.js
+++ wander.js
@@ -40,6 +40,59 @@ function passwordCheck(){
 
 }
 
+let formValueCheck = [];
+
+
+const phoneFormater = document.querySelector('#phone_num');
+phoneFormater.addEventListener('keyup', (e) => {
+
+    // Remove anything that isn't a number
+    let phoneNumber = phoneFormater.value.replace(/\D/g,'');
+    
+    let inputSize = phoneNumber.length;
+
+    // Ignore delete/backspace so user can delete number 
+    if (e.key == 'Backspace' || e.key == 'Delete') return;
+    
+
+    if (inputSize >= 3 && inputSize < 7 && e.key != 'Backspace'){
+        phoneNumber = phoneNumber.substring(0,3) + '-' + phoneNumber.substring(3)
+    }
+    else if (inputSize >= 7){
+        phoneNumber = phoneNumber.substring(0,3) + '-' + phoneNumber.substring(3,6) + '-' + phoneNumber.substring(6)
+    }
+
+
+    phoneFormater.value = phoneNumber;
+})
+
+// If an input is valid, add its value to an array
+const formelements = document.querySelectorAll('.form-element');
+
+    formelements.forEach(input => {
+        input.addEventListener('change', () => {
+            console.log('change')
+            if (input.checkValidity()){
+                console.log('valid')
+            formValueCheck[input.name] = input.value;
+            verifyForm();
+            }
+
+        })
+    });
+    
+// If our verified values array object is the same length as 
+// the number of inputs, allow submission
+function verifyForm(){
+    console.log("checking")
+    console.log(Object.keys(formValueCheck).length + " " + formelements.length)
+    if (Object.keys(formValueCheck).length == formelements.length){
+        
+        document.getElementById('create-account').classList.remove('inactive');
+    }
+}
+
+// These are broken up into helper functions for future scalibility
 function addError(element){
     element.classList.add('error');
 }
@@ -47,16 +100,3 @@ function addError(element){
 function removeError(element){
     element.classList.remove('error');
 }
-
-
-
-
-const formelements = document.querySelectorAll('.form-element');
-    formelements.forEach(input => {
-        console.log(input);
-    console.log(input.valid)
-    
-        return input.validityState
-    });
-
-    console.log(formelements)
